0d335913ce67236b32038af0343172eca41e507f
StrigZ <mirgasan2010@ya.ru>
2022-03-30 15:28:24 +0300

    swapped from jq slide to vanilla js

--- js/index.js
+++ js/index.js
@@ -5,35 +5,23 @@
       crossorigin="anonymous"
     ></script> */
 }
-//Poor perfomance on the Phone
 
 $(".accordion__panel").click(function (e) {
   e.preventDefault();
-  for (let el of $(".accordion__panel")) {
-    if (this !== el && el.lastElementChild.style.maxHeight) {
-      //Poor perfomance on the Phone
-      // $(el.lastElementChild).slideUp(200);
-      $(el.firstElementChild).removeClass("active");
+  let h2 = this.firstElementChild;
+  let p = this.lastElementChild;
+
+  for (let e of $(".accordion__panel")) {
+    if (e !== this) {
+      $(e.firstElementChild).removeClass("active");
+      $(e.lastElementChild).css("max-height", "0");
     }
-  }
-  if (!$(this).hasClass("active")) {
-    //Poor perfomance on the Phone
-    // $(this.lastElementChild).slideToggle(200);
-    $(this.firstElementChild).toggleClass("active");
-    $(this.lastElementChild).css(
-      "max-height",
-      $(this.lastElementChild)[0].scrollHeight + "px"
-    );
-  } else {
-    $(this.lastElementChild).style.maxHeight =
-      $(this.lastElementChild).scrollHeight + "px";
   }
 
-  if ($(this.lastElementChild).css(
-      "max-height")) {
-      $(this.lastElementChild).css(
-      "max-height") = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
+  $(h2).toggleClass("active");
+
+  if ($(p).css("max-height")[0] === "0") {
+    // console.log($(p)[0].scrollHeight + "px");
+    $(p).css("max-height", $(p)[0].scrollHeight + "px");
+  }
 });
