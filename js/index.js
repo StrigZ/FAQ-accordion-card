4d96f3beabf5abc31a6c6d9fcb297dc2e0bb0d49
StrigZ <mirgasan2010@ya.ru>
2022-03-30 14:37:26 +0300

    commit

--- js/index.js
+++ js/index.js
@@ -5,16 +5,35 @@
       crossorigin="anonymous"
     ></script> */
 }
+//Poor perfomance on the Phone
+
 $(".accordion__panel").click(function (e) {
   e.preventDefault();
   for (let el of $(".accordion__panel")) {
-    if (this !== el && el.lastElementChild.style.display === "block") {
-      $(el.lastElementChild).slideUp(200);
+    if (this !== el && el.lastElementChild.style.maxHeight) {
+      //Poor perfomance on the Phone
+      // $(el.lastElementChild).slideUp(200);
       $(el.firstElementChild).removeClass("active");
     }
   }
   if (!$(this).hasClass("active")) {
+    //Poor perfomance on the Phone
+    // $(this.lastElementChild).slideToggle(200);
     $(this.firstElementChild).toggleClass("active");
-    $(this.lastElementChild).slideToggle(200);
+    $(this.lastElementChild).css(
+      "max-height",
+      $(this.lastElementChild)[0].scrollHeight + "px"
+    );
+  } else {
+    $(this.lastElementChild).style.maxHeight =
+      $(this.lastElementChild).scrollHeight + "px";
   }
+
+  if ($(this.lastElementChild).css(
+      "max-height")) {
+      $(this.lastElementChild).css(
+      "max-height") = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
 });
